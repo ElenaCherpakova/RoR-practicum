1b1098dd3319306bfcd89db58702a1b182ba85ad
AigulY <azia.kz.a@gmail.com>
2024-03-15 17:06:51 -0400

    Add name validation for City model and corresponding tests

--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class CityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid city' do
+    city = cities(:one)
+    assert city.valid?
+  end
+
+  test 'invalid without name' do
+    city = City.new
+    refute city.valid?, 'city is valid without a name'
+    assert_not_nil city.errors[:name], 'no validation error for name present'
+  end
 end
