9e121e2c3f63fcc4bccf490e00fa3621e667cadc
Ron Javellana <Ronapj1991@gmail.com>
2024-03-20 23:07:51 -0400

    the index method is an example of how to use the Faraday gem

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,11 @@
 class PlacesController < ApplicationController
-    private
+  #this method is not final. It only sets an example on how to call api using the Faraday gem
+  def index
+    response = Faraday.get("https://api.geoapify.com/v2/places?categories=commercial.shopping_mall&filter=place:51ed56e07547c754c059a9b64027292a4040f00101f901a578020000000000c0020a&limit=10&apiKey=ab5ac1201aa842a0b029f913eea34500")
+  end
+
+  
+  private
     def place_params
       params.require(:place).permit(:name, :address, :place_type, :description, :rating, :api_id, :image_url, :city_id)
     end
