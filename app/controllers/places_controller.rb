8a9c5ae504ad8ec113ee78523097248848e2bfaf
Ron Javellana <Ronapj1991@gmail.com>
2024-03-23 20:59:30 -0400

    refactored faraday useage and removed yaml file

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,10 +2,13 @@ class PlacesController < ApplicationController
   #this method is not final. It only sets an example on how to call api using the Faraday gem
   def index
     debugger
-    response = Faraday.get("https://api.geoapify.com/v2/places?categories=commercial.shopping_mall&filter=place:51ed56e07547c754c059a9b64027292a4040f00101f901a578020000000000c0020a&limit=10&apiKey=" + ENV['PLACES_API_KEY'])
+    response = GeoapifyClient.connection.get("/v2/places") do |req|
+      req.params["categories"] = "commercial.shopping_mall"
+      req.params["filter"] = "place:51ed56e07547c754c059a9b64027292a4040f00101f901a578020000000000c0020a"
+      req.params["limit"] = 5
+    end
   end
 
-  
   private
     def place_params
       params.require(:place).permit(:name, :address, :place_type, :description, :rating, :api_id, :image_url, :city_id)
