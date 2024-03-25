7d529b158e17b0b3eb1be7a8b7f3946a15e483b8
ElenaCherpakova <elena.cherpakova@gmail.com>
2024-03-25 19:37:27 -0400

    edit based on notes

--- db/migrate/20240320185213_create_collections.rb
+++ db/migrate/20240320185213_create_collections.rb
@@ -1,7 +1,5 @@
 class CreateCollections < ActiveRecord::Migration[7.1]
   def change
-    return if table_exists?(:collections)
-
     create_table :collections do |t|
       t.string :name, null: false
       t.references :user, null: false, foreign_key: true
