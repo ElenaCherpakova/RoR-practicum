5656d94795725f0291769e3bdc91ef558be3714a
Ron Javellana <Ronapj1991@gmail.com>
2024-03-26 12:04:37 -0400

    started from scratch without rack-cors gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_22_022831) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_26_160216) do
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -46,6 +46,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_22_022831) do
     t.datetime "updated_at", null: false
     t.integer "city_id", null: false
     t.index ["city_id"], name: "index_places_on_city_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
